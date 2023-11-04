feb9835fa52f8b0b7fbeef1def7e983e415885d8
Joshua Hecker <jhecker2001@gmail.com>
2023-11-04 10:31:20 -0500

    Arranged necessary elements on tkinter grid.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from tkinter import *
 
+window = Tk()
+window.title("Miles to KM Converter")
+window.config(padx=20, pady=20)
+
+miles_entry = Entry(width=10)
+miles_entry.grid(column=1, row=0)
+miles_entry.insert(END, string="0")
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+equal_to = Label(text="is equal to")
+equal_to.grid(column=0, row=1)
+
+result = Label(text="0")
+result.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+calc_button = Button(text="Calculate")
+calc_button.grid(column=1, row=2)
+
+window.mainloop()
